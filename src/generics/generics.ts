e0b4b2f364511ef438af2047de1dd04ea2e553d6
Chris Frewin <frewin.christopher@gmail.com>
2021-01-22 11:08:05 +0100

    lesson-4 - second example of using generics

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -60,3 +60,43 @@ sortByKey<IFooBar>(fooBars, "bar")
 
 // TypeScript complains: cat is not a property of IFooBar!
 sortByKey<IFooBar>(fooBars, "cat")
+
+class Animal {
+    public legCount: number;
+    constructor(legCount: number) {
+        this.legCount = legCount
+    }
+}
+
+class Cat extends Animal {
+    constructor() {
+        super(4);
+    }
+}
+
+class Kangaroo extends Animal {
+    constructor() {
+        super(2);
+    }
+}
+
+class Bacteria {
+
+}
+
+function printLegCount<T extends Animal>(animal: T) {
+    console.log(`My leg count is: ${animal.legCount}`);
+}
+
+const myCat = new Cat();
+
+const myKangaroo = new Kangaroo();
+
+printLegCount(myCat);
+
+printLegCount(myKangaroo);
+
+const myBacteria = new Bacteria();
+
+// TypeScript complains, since the Bacteria class does not extend animal!
+printLegCount(myBacteria);
